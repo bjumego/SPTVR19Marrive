3d557f67c710382fd17fd2529622f04fc07d99fa
Sergei <sergei.smirnov001@ivkhk.ee>
2020-09-16 12:27:33 +0300

    выдает среднее арифметическое элементов без учета мин. и макс. элементов массива

--- src/sptvr19marrive/App.java
+++ src/sptvr19marrive/App.java
@@ -12,16 +12,32 @@ import java.util.Random;
  * @author user
  */
 class App {
-    public void run(){
-        System.out.println("--- Marrive ---");
-         Random random = new Random();
+    public void run() {
+        System.out.println("-----Массивы-----");
+        Random random = new Random();
+        int rnum;
+        rnum = 0;
+        int min = 100;
+        int max = 0;
+        int sum = 0;
         //Создаём массив чётных целых чисел.
         int [] numbers = new int [20];
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = random.nextInt(99-0+1)+0;
+            while (true) {
+                rnum = random.nextInt(99-0+1)+0;
+                if (rnum %2 == 0) {
+                    numbers[i] = rnum;
+                    break;
+                }
+            }
+            if (numbers[i] < max)max = numbers[i];
+            if (numbers[i] > min)min = numbers[i];
+            sum += numbers[i];
             System.out.printf("%4d", numbers[i]);
          
         }
         System.out.println();
+        System.out.println("Среднее арифметическое без учёта минимального(+min+) и максимального(+max+)");
+        System.out.println((double)(sum-min-max)/(numbers.length - 2));
     }
 }
